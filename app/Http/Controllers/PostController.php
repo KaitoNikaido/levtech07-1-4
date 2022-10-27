74af0c6b8ece8f9d456052688324c79df475c58f
KaitoNikaido <kaito.syukathu@gmail.com>
2022-10-27 20:43:12 +0900

    08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,18 +7,17 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    //use宣言は外部にあるクラスをPostController内にインポートできる。
-    //この場合、App\Models内のPostクラスをインポートしている。
-    /**
-     * Post一覧を表示する
-     * 
-     * @param Post Postモデル
-     * @return array Postモデルリスト
+     /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
      */
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function show(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
-    }
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }  
+ 
 }
 ?>
